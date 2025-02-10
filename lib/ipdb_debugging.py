feae6aab63370fb66db3dd0b9751e8131e027cff
ronaldochirchir <ronaldochirchir8@gmail.com>
2025-02-10 23:53:08 +0300

    completed laps

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python3
-
-import ipdb
-
 def plus_two(num):
-    num + 2
+    num += 2
     return num
--- lib/testing/ipdb_debugging_test.py
+++ lib/testing/ipdb_debugging_test.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python3
+from lib.ipdb_debugging import plus_two
 
-from ipdb_debugging import plus_two
-
-class TestIpdbDebugging:
-    '''ipdb_debugging.py'''
-    
-    def test_adds_two(self):
-        '''adds_two() adds 2 to input arg and returns sum.'''
-        assert(plus_two(3) == 5)
+def test_adds_two():
+    '''adds_two() adds 2 to input arg and returns sum'''
+    assert plus_two(3) == 5
